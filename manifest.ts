7cb27e1db8dcd0ba8e52f1293c1b4052b1e570e3
degenRobot <86513395+degenRobot@users.noreply.github.com>
2023-08-21 09:43:26 +0800

    Initial commit

---
+++ manifest.ts
@@ -0,0 +1,18 @@
+import { Manifest } from 'https://deno.land/x/robo_arkiver@v0.4.21/mod.ts'
+import { VaultSnapshot } from './entities/vault.ts'
+import { snapshotVault } from './handlers/vault.ts'
+
+const manifest = new Manifest('yearn-vaults')
+
+manifest
+  .addEntity(VaultSnapshot)
+  .addChain('mainnet', (chain) =>
+    chain
+      .addBlockHandler({
+        blockInterval: 1000,
+        startBlockHeight: 12790000n,
+        handler: snapshotVault,
+      }))
+
+export default manifest
+  .build()
